14a47604e70e32d1a690ce37842e962f84520337
carlosmartde <160431626+carlosmartde@users.noreply.github.com>
2025-05-24 12:02:36 -0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
-
-
+use Illuminate\Support\Facades\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,10 +19,16 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
-{
-    if (env('APP_ENV') !== 'production') {
-        URL::forceScheme('https');
+    public function boot(): void
+    {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+
+            // Confiar en los proxies al usar servicios como Railway
+            Request::setTrustedProxies(
+                [$this->app['request']->getClientIp()],
+                \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL
+            );
+        }
     }
-}
 }
